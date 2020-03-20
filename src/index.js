d12684b6c3ad63d5ca3a94ae9463c93899ebdb14
Tanya Tomina <tanya.tomina@fl3xx.com>
2020-03-20 15:55:42 +0300

    feat: Add reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n >= 0) {
+      return n.toString().split('').reverse().join('');
+  } else {
+      n = n.toString().split('');
+      let removed = n.shift();
+      n = n.reverse().join('');
+      return n;
+  }
 }
